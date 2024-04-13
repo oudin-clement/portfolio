61987edf3bad852b0db5b687b54689133cb8eec5
oudin-clement <clementoudin3@gmail.com>
2024-04-13 17:37:38 +0200

    portfolio

---
+++ pages/index.vue
@@ -0,0 +1,233 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="w-50 mx-auto">
+    <h1 class="text-center mt-13">Portfolio Clément Oudin</h1>
+    <p class="text-justify mt-13">
+      J'ai suivi un BUT en informatique. Le contenu du programme comprenait des cours théoriques couvrant les
+      fondamentaux de l'informatique, tels que les langages de programmation, les bases de données, les systèmes
+      d'exploitation, etc. Il y avait également des cours pratiques où nous avons travaillé sur des projets concrets, ce
+      qui nous a permis d'appliquer les connaissances acquises en classe.
+      <br>
+      En ce qui concerne les stages en entreprise, j'ai eu l'opportunité d'en effectuer deux au cours de mon parcours.
+      Ces stages m'ont permis de mettre en pratique ce que j'avais appris en classe, d'acquérir de nouvelles compétences
+      et de découvrir le fonctionnement du monde professionnel de l'informatique.
+      <br>
+      Globalement, le programme du BUT correspondait à mes attentes. Il m'a fourni une base solide en informatique, à la
+      fois théorique et pratique, et m'a préparé efficacement à intégrer le marché du travail dans ce domaine. Les
+      stages en entreprise ont été particulièrement bénéfiques pour ma formation, car ils m'ont permis de développer des
+      compétences professionnelles et d'établir des contacts dans l'industrie.
+      <br>
+      En résumé, je considère que le BUT a été une expérience enrichissante et pertinente pour mon développement
+      professionnel.
+    </p>
+
+    <v-table class="mt-13">
+      <thead>
+      <tr>
+        <th class="text-left">
+          Compétences générales
+        </th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td>Réaliser des applications : ★★★★</td>
+        <td>J'ai une solide expérience en programmation et en conception orientée objet, ainsi qu'en analyse des besoins
+          et en test unitaire. J'ai participé à plusieurs projets de développement d'applications, ce qui m'a permis de
+          renforcer mes compétences dans ce domaine.
+        </td>
+      </tr>
+      <tr>
+        <td>Optimiser des applications : ★★★</td>
+        <td>Je possède une bonne compréhension des algorithmes itératifs et récursifs, ainsi que des structures de
+          données classiques. J'ai utilisé des outils mathématiques pour analyser les performances des applications,
+          mais je pourrais approfondir mes connaissances dans ce domaine.
+        </td>
+      </tr>
+      <tr>
+        <td>Administrer les applications : ★★</td>
+        <td>J'ai des connaissances de base en administration système et en configuration, mais je pourrais encore
+          améliorer mes compétences dans ce domaine en acquérant une expérience pratique supplémentaire.
+        </td>
+      </tr>
+      <tr>
+        <td>Gérer des données : ★★★</td>
+        <td>J'ai une bonne compréhension de la modélisation des données et de la programmation SQL, ainsi que de
+          l'utilisation des transactions. J'ai travaillé sur plusieurs projets impliquant la manipulation de données, ce
+          qui m'a permis de consolider mes compétences dans ce domaine.
+        </td>
+      </tr>
+      <tr>
+        <td>Conduire un développement : ★★★★</td>
+        <td>
+          J'ai une bonne expérience de travail en projet et je suis familier avec les méthodologies agiles. J'ai été
+          impliqué dans la gestion des besoins des clients et dans la coordination des équipes de développement.
+        </td>
+      </tr>
+      <tr>
+        <td>Collaborer : ★★★★</td>
+        <td>Je communique efficacement en français et en anglais, et je suis à l'aise pour travailler en équipe. J'ai
+          partagé des documents et du code avec mes collègues et j'ai été apprécié pour ma capacité à collaborer de
+          manière productive.
+        </td>
+      </tr>
+
+      </tbody>
+    </v-table>
+
+    <v-table class="mt-13">
+      <thead>
+      <tr>
+        <th>
+          Compétences spécifiques liées au développement web et mobile
+        </th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td>
+          Connaître l’architecture d’une application web distribuée : ★★★
+        </td>
+        <td>
+          J'ai une bonne compréhension de l'architecture des applications web distribuées et des services API, mais je
+          pourrais approfondir mes connaissances dans ce domaine en étudiant des cas d'utilisation plus avancés.
+        </td>
+      </tr>
+      <tr>
+        <td>Concevoir, développer et déployer des composants côté serveur : ★★★</td>
+        <td>
+          J'ai développé des composants côté serveur dans le cadre de plusieurs projets, en utilisant les bonnes
+          pratiques et les outils appropriés. J'ai une bonne compréhension de ces concepts, mais je pourrais encore
+          améliorer mes compétences en les appliquant à des scénarios plus complexes.
+        </td>
+      </tr>
+      <tr>
+        <td>Concevoir, développer et déployer des composants côté client : ★★★</td>
+        <td>
+          J'ai une expérience significative dans le développement de composants côté client, en appliquant les bonnes
+          pratiques et en tenant compte du contexte d'utilisation. Je suis à l'aise avec les outils et les langages
+          utilisés dans ce domaine.
+        </td>
+      </tr>
+      <tr>
+        <td>Mettre en œuvre des systèmes d'information à base de services et micro-services : ★★</td>
+        <td>
+          J'ai une compréhension de base des architectures à base de services et de micro-services, mais je pourrais
+          approfondir mes connaissances en étudiant des cas d'utilisation plus avancés et en acquérant une expérience
+          pratique supplémentaire.
+        </td>
+      </tr>
+      <tr>
+        <td>Travailler en utilisant les outils et pratiques courants pour la collaboration : ★★★★</td>
+        <td>
+          J'ai une bonne maîtrise des outils de collaboration courants tels que Git et des pratiques telles que la
+          gestion de code et des versions. J'utilise régulièrement ces outils dans mes projets et je suis à l'aise pour
+          travailler dans des environnements de développement collaboratif.
+        </td>
+      </tr>
+      <tr>
+        <td>Analyser et mettre en œuvre les besoins et contraintes non fonctionnels dans le développement : ★★★</td>
+        <td>J'ai une compréhension solide des besoins et des contraintes non fonctionnels dans le développement,
+          notamment en matière de sécurité, de performances et d'ergonomie. J'ai appliqué ces connaissances dans mes
+          projets et je suis capable de les mettre en œuvre de manière efficace.
+        </td>
+      </tr>
+      </tbody>
+    </v-table>
+
+    <v-table class="mt-13 mb-13">
+      <thead>
+      <tr>
+        <th>
+          Connaissances techniques en programmation et développement
+        </th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td>Intégration web et interfaces : ★★★★</td>
+        <td>
+          J'ai une solide expérience en HTML5/CSS3, ainsi qu'en utilisation de frameworks CSS et de processeurs CSS.
+          J'ai travaillé sur plusieurs projets impliquant le développement d'interfaces utilisateur et je suis à l'aise
+          pour utiliser ces technologies.
+        </td>
+      </tr>
+      <tr>
+        <td>Développement frontend : ★★★</td>
+        <td>
+          J'ai une bonne maîtrise de la programmation JavaScript et de l'accès aux services web. J'ai utilisé des
+          frameworks frontend comme Vue.js dans mes projets et je suis capable de développer des interfaces utilisateur
+          interactives et réactives.
+        </td>
+      </tr>
+      <tr>
+        <td>Développement mobile : ★★</td>
+        <td>
+          J'ai une certaine expérience dans le développement d'applications mobiles hybrides et natives, mais je
+          pourrais approfondir mes compétences dans ce domaine en travaillant sur des projets plus avancés et en
+          acquérant une expérience pratique supplémentaire.
+        </td>
+      </tr>
+      <tr>
+        <td>Développement backend : ★★★</td>
+        <td>
+          J'ai une bonne compréhension de l'architecture backend et de la programmation PHP et Node.js. J'ai développé
+          des applications backend dans le cadre de plusieurs projets et je suis à l'aise pour utiliser des frameworks
+          comme Slim et Express.js.
+        </td>
+      </tr>
+      <tr>
+        <td>Services web : ★★★</td>
+        <td>
+          J'ai une bonne maîtrise des APIs et des principes REST, ainsi que des formats et échanges de données. J'ai
+          travaillé sur plusieurs projets impliquant la mise en œuvre de services web et je suis capable de concevoir et
+          de développer des APIs efficaces.
+        </td>
+      </tr>
+      <tr>
+        <td>Gestion de données : ★★★</td>
+        <td>
+          J'ai une bonne compréhension de SQL et de l'utilisation d'un ORM, ainsi que de l'utilisation de bases de
+          données NoSQL comme MongoDB et Redis. J'ai manipulé des données dans le cadre de plusieurs projets et je suis
+          à l'aise pour utiliser ces technologies.
+        </td>
+      </tr>
+      <tr>
+        <td>Gestion du code : ★★★★</td>
+        <td>
+          J'ai une bonne maîtrise de Git et des outils de documentation, ainsi que des linters et analyses de code.
+          J'utilise régulièrement ces outils dans mes projets et je suis capable de les utiliser de manière efficace
+          pour gérer le code.
+        </td>
+      </tr>
+      <tr>
+        <td>Gestion de projets : ★★★★</td>
+        <td>
+          J'ai une bonne compréhension de l'analyse des besoins, de la conception et des patterns, ainsi que de la
+          gestion de projet agile. J'ai travaillé sur plusieurs projets en utilisant ces pratiques et je suis capable de
+          les appliquer de manière efficace dans mes projets.
+        </td>
+      </tr>
+      <tr>
+        <td>Administration et déploiement : ★★★</td>
+        <td>
+          J'ai une certaine expérience dans la virtualisation des services et l'utilisation de Docker, ainsi que dans
+          l'administration HTTP/HTTPS et le déploiement cloud. Je pourrais approfondir mes compétences dans ce domaine
+          en acquérant une expérience pratique supplémentaire.
+        </td>
+      </tr>
+      </tbody>
+    </v-table>
+
+  </div>
+</template>
+
+<style scoped>
+.v-table {
+  border: black solid 1px;
+  border-radius: 10px;
+}
+</style>
